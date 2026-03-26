1861f05c244eb8856d9e756ea9c613c98eef4a9d
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2026-03-26 21:22:34 +0000

    edit server

--- chat-app/backend/server.js
+++ chat-app/backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
+app.use(cors());
 let messages = [];
 
 app.get("/messages", (req, res) => {
