10d408ba283160aed9b8ab2df21c28560b62c239
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2026-08-05 20:44:05 +0100

    Fix server.js after merge

--- chat-app/backend/server.js
+++ chat-app/backend/server.js
@@ -3,29 +3,20 @@ import cors from "cors";
 
 const app = express();
 const PORT = 3000;
-<<<<<<< HEAD
 
-app.use(express.json());
-app.use(cors());
-
-const messages = [];
-=======
 app.use(express.json());
 app.use(cors());
 let messages = [];
->>>>>>> sprint3-middleware
 
 app.get("/messages", (req, res) => {
   res.json(messages);
 });
 
+app.get("/messages", (req, res) => {
+  res.json(messages);
+});
 app.post("/messages", (req, res) => {
-<<<<<<< HEAD
-  const { username, text } = req.body;
-  const message = { username, text, timeStamp: new Date().toISOString() };
-=======
   const message = req.body;
->>>>>>> sprint3-middleware
   messages.push(message);
   res.status(201).json(message);
 });
